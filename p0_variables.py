daccb2e81041912f52f8c14af981dfde547bb739
Qaeef250f <161265446+Qaeef250f@users.noreply.github.com>
2024-03-08 11:52:11 +1300

    variables basics

---
+++ p0_variables.py
@@ -0,0 +1,19 @@
+# code to learn about variables...
+
+#variables can be a string (note the speech marks)
+greetings = "hello world"
+
+#variables can be a number
+fav_num = 7
+num_string ="5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
+# constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7 
+
+
+
